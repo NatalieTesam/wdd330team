6aa74a1ae0c101528c2e56ac0d18e2eebabc9d7c
tylerburdett7 <bur23034@byui.edu>
2025-09-30 14:11:42 -0600

    fixed add to cart functionality

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,17 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cart = getLocalStorage("so-cart") || [];
+  // if cart is not an array (e.g., from older saves), reset it
+  if (!Array.isArray(cart)) {
+    cart = [];
+  }
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
 }
+
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
