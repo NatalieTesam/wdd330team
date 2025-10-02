ad8397cd0a05bd72a320c8e5094c9efaf2411dc5
gabriel-wargha <gabriel@wargha.com>
2025-10-02 08:56:29 -0600

    feat: mostra total do carrinho quando houver itens

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,23 @@
 import { getLocalStorage } from "./utils.mjs";
 
+const cartTotal = document.querySelector(".cart-total");
+const cartFooter = document.querySelector(".cart-footer");
+
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  let total = 0;
+
+  if (cartItems.length > 0) {
+    cartTotal.classList.remove("hide");
+    cartFooter.classList.remove("hide");
+
+    cartItems.forEach((item) => {
+      total += item.FinalPrice;
+    });
+  }
+  cartTotal.innerHTML = `Total: $${total.toFixed(2)}`;
 }
 
 function cartItemTemplate(item) {
