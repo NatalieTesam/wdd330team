f4244cb8aabdc667e67159de30e210abdf843b52
Tara Bergener <tara.kelly211@gmail.com>
2025-10-31 01:16:58 -0400

    fixed errors in product-list index.html

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -1,7 +1,5 @@
-import productList, { sortContent } from "./productList.mjs";
+import productList from "./productList.mjs";
 import { getParam } from "./utils.mjs";
 
 const category = getParam("category");
 productList(".product-list", category);
-
-sortContent();
